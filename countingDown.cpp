213f31aca43c4c8c75b26f4ed192a74cd5e9f6fb
ZeidLennart <lennyzeidler@gmail.com>
2023-10-18 12:33:27 +0200

    final version of countingDown

--- countingDown.cpp
+++ countingDown.cpp
@@ -1,12 +1,66 @@
 #include <iostream>
 using namespace std;
 
-char countDown(string input)
+string countDown(string input)
 {
-    return (input.back());
+    char lastChar = input.back();
+    input.pop_back();
+    if (lastChar == '0')
+    {
+        return countDown(input) + "1";
+    }
+    else
+    {
+        string output = input + '0';
+        return output;
+    }
 }
 
-char main()
+bool checkIfZ(string input)
 {
-    cout << countDown("Test");
+    bool result = true;
+    for (int i = 0; i < input.size(); i++)
+    {
+        if (input[i] == '1')
+        {
+            result = false;
+            break;
+        }
+    }
+    return result;
+}
+
+bool isBinary(string input)
+{
+    bool result = true;
+    for (int i = 0; i < input.size(); i++)
+    {
+        if (!(input[i] == '1' or input[i] == '0'))
+        {
+            result = false;
+            break;
+        }
+    }
+    return result;
+}
+
+int main()
+{
+    string input = "0";
+    cout << "enter a binary number" << endl;
+    cin >> input;
+    if (!isBinary(input))
+    {
+        cout << "not binary input" << endl;
+    }
+    else
+    {
+        bool isNull = checkIfZ(input);
+        while (!isNull)
+        {
+            input = countDown(input);
+            isNull = checkIfZ(input);
+            cout << input << endl;
+        }
+    }
 }
